625d3cf915c393f298bf232b99200b0409f5b71b
FattyMrPiggy <cheungyungkong@gmal.com>
2017-06-03 11:14:02 +0800

    Phaser

--- Phaser(Lesson 7)/app.js
+++ Phaser(Lesson 7)/app.js
@@ -143,21 +143,21 @@ function update(){
 		enemy1.body.velocity.x = -300;
 	} else if(enemy1.x < 405){
 		enemy1.animations.play('right');
-		enemy1.body.velocity.x = x300;
+		enemy1.body.velocity.x = 300;
 	}
 	if(enemy2.x > 200){
 		enemy2.animations.play('left');
 		enemy2.body.velocity.x = -300;
 	} else if(enemy2.x < 21){
 		enemy2.animations.play('right');
-		enemy2.body.velocity.x = x300;
+		enemy2.body.velocity.x = 300;
 	}
 	if(enemy3.x > 759){
 		enemy3.animations.play('left');
 		enemy3.body.velocity.x = -300;
 	} else if(enemy3.x < 51){
 		enemy3.animations.play('right');
-		enemy3.body.velocity.x = x300;
+		enemy3.body.velocity.x = 300;
 	}
 
 	//Collide Stars with platforms
